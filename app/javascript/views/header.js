3fc41e20b4e6fce73ff14a71c56f90f4a16f8e63
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-09-14 10:53:35 +0000

    [Add]hm-icon finish

--- app/javascript/views/header.js
+++ app/javascript/views/header.js
@@ -1,7 +1,8 @@
 $(function() {
   $('.menu-trigger').on('click', function(event) {
     $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
+    // $('#sp-menu').fadeToggle();
+    $('#sp-menu').toggleClass('menu-active');
     event.preventDefault();
   })
 })
