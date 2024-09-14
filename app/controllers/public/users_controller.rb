e4326084b477b4984468b84dd6f3ff8505f8328c
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-09-14 19:43:54 +0000

    [Add]user_introduction show,edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,15 +3,17 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def show
     @user = User.find(params[:id])
-    
+
   end
 
   def update
-
+    user = User.find(params[:id])
+    @user.update(user_params)
   end
 
   def destroy
@@ -20,5 +22,11 @@ class Public::UsersController < ApplicationController
 
   def favorite
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :is_active)
   end
 end
