54e1b897f7759ede31286f7dcf3af7edc1dbbed2
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-09-15 03:39:28 +0000

    [Add]users show edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -13,7 +13,9 @@ class Public::UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
-    @user.update(user_params)
+    if user.update(user_params)
+      redirect_to user_path(user.id)
+    end
   end
 
   def destroy
