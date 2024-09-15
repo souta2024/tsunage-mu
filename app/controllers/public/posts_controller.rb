cdf7227d777fa4cd523204dfa334ce2c179baafe
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-09-15 15:52:05 +0000

    [Add]create post

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,9 +1,16 @@
 class Public::PostsController < ApplicationController
   def index
+    @post = Post.new
   end
 
   def create
-
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+    if post.save
+      redirect_to post_path(post.id)
+    else
+      redirect_to root_path
+    end
   end
 
   def show
@@ -21,5 +28,11 @@ class Public::PostsController < ApplicationController
   end
 
   def update_history
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :is_active)
   end
 end
