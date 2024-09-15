6b4a4084c9a28c2a5c7ebad0e179f6ef20a4def6
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-09-15 13:30:19 +0000

    [Fix]turbolinks invalidate

--- app/javascript/views/header.js
+++ app/javascript/views/header.js
@@ -1,8 +1,8 @@
-document.addEventListener('turbolinks:load', $(function() {
-    $('.menu-trigger').on('click', function(event) {
-      $(this).toggleClass('active');
-      // $('#sp-menu').fadeToggle();
-      $('#sp-menu').toggleClass('menu-active');
-      event.preventDefault();
-    });
-  }));
+$(document).on('turbolinks:load', function() {
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    // $('#sp-menu').fadeToggle();
+    $('#sp-menu').toggleClass('menu-active');
+    event.preventDefault();
+  })
+})
