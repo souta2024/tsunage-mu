0c03c2a58727345480c08634983520c6c4c9bfef
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-09-16 05:51:52 +0000

    [Add,Layout]update validates, show

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -8,12 +8,15 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_posts = @user.posts
   end
 
   def update
     user = User.find(params[:id])
     if user.update(user_params)
       redirect_to user_path(user.id)
+    else
+      render :edit
     end
   end
 
