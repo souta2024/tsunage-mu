6bc19aba0d3f66c4b3d657908c2bbee23097d3f8
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-09-16 07:27:23 +0000

    [Add]validates user post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  validates :body, presence: true, length: {in: 1..1000}
+
   has_many :comments
   has_many :post_favorites, dependent: :destroy
   has_many :post_histories, dependent: :destroy
