67410896ac3303912ee647253edd4e659fbffe75
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-09-26 01:04:20 +0000

    [Add]following

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -13,11 +13,11 @@ class Public::RelationshipsController < ApplicationController
 
   def followings
     user = User.find_by(account_id: params[:account_id])
-    @users = user.followings
+    @users = user.followings.where(is_active: true)
   end
 
   def followers
     user = User.find_by(account_id: params[:account_id])
-    @user = user.followers
+    @users = user.followers.where(is_active: true)
   end
 end
