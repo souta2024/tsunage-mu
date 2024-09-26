c7433cc9e1f994dcd107889a3d3d3d1b15c82d94
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-09-26 15:58:55 +0000

    [Add]search

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,4 +1,15 @@
 class Public::SearchesController < ApplicationController
   def search
+    @range = params[:range]
+    @word = params[:word]
+    @search = params[:search]
+
+    if @range == "user"
+      @records = User.where(is_active: true).search_for_name(@word, @search)
+    elsif @range == "account_id"
+      @records = User.where(is_active: true).search_for_account_id(@word, @search)
+    else
+      @records = Post.where(is_draft: false, is_hidden: false).search_for_post(@word, @search)
+    end
   end
 end
