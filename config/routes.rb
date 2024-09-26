cdc8f6f85c2b1561e4033eff92d987b897d6fc3a
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-09-26 18:39:35 +0000

    [Add]guest

--- config/routes.rb
+++ config/routes.rb
@@ -60,5 +60,10 @@ Rails.application.routes.draw do
   devise_for :admins,skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "public/sessions#guest_sign_in"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
