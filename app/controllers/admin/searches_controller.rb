3f852c94aad9af37a99dbff98bb9e572a10b8e86
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-09-28 12:09:43 +0000

    [Add]admin search_order_fix

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,17 +1,17 @@
 class Admin::SearchesController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def search
     @range = params[:range]
     @word = params[:word]
     @search = params[:search]
 
     if @range == "user"
-      @records = User.where.not(name: "guestuser").search_for_name(@word, @search)
+      @records = User.where.not(name: "guestuser").search_for_name(@word, @search).order(created_at: :desc).page(params[:page]).per(1)
     elsif @range == "account_id"
-      @records = User.where.not(name: "guestuser").search_for_account_id(@word, @search)
+      @records = User.where.not(name: "guestuser").search_for_account_id(@word, @search).order(created_at: :desc).page(params[:page]).per(1)
     else
-      @records = Post.where(is_draft: false).search_for_post(@word, @search)
+      @records = Post.where(is_draft: false).search_for_post(@word, @search).order(published_at: :desc).page(params[:page]).per(1)
     end
   end
 end
